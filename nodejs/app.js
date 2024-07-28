d9386b45740e6e8dbe5f2e90747d25dfece9284e
Sarika Marwah <sarika.marwah@mavenwave.com>
2024-07-28 21:43:54 +0530

    express and cors

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,1 +1,17 @@
-console.log("hi");
+let express = require('express');
+let app = express();
+let port = 9110;
+let cors = require('cors');
+
+app.use(cors())
+app.get('/',(req,res) => {
+    res.send("Hi from server to you");
+})
+app.get('/location',(req,res)=>{
+    res.send("hi from location")
+})
+
+app.listen(port,(err)=>{
+    if(err) throw err;
+    console.log(`server is running on the port ${port}`);
+})
